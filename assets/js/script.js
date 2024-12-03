d41af548233aa3d4a9452c51fb853ce9f90e69aa
Xavier <xaviervang1999@gmail.com>
2024-12-02 20:58:31 -0600

    Added fav buttons to ALL pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,9 +81,156 @@ let Location5 = {
 }
 
 // SA Locations
+let Location6 = {
+    'country': 'SA', 
+    'description': 'Argentine Patagonia',
+    'image': '../Images/Argentine Patagonia.jpeg',
+    'buttonid': '6LocButton'
+}
+
+let Location7 = {
+    'country': 'SA', 
+    'description': "Chilean Patagonia",
+    'image': '../Images/Chilean Patagonia.jpeg',
+    'buttonid': '7LocButton'
+}
+
+let Location8 = {
+    'country': 'SA', 
+    'description': 'Costa Rica',
+    'image': '../Images/Costa Rica.jpeg',
+    'buttonid': '8LocButton'
+}
+
+let Location9 = {
+    'country': 'SA', 
+    'description': 'Galápagos Islands',
+    'image': '../Images/Galapagos Islands.jpeg',
+    'buttonid': '9LocButton'
+}
+
+let Location10 = {
+    'country': 'SA', 
+    'description': "Chilean Patagonia",
+    'image': '../Images/Torres del Paine National Park.jpeg',
+    'buttonid': '10LocButton'
+}
+
+// Europe Locations
+let Location11 = {
+    'country': 'EU', 
+    'description': 'Paris',
+    'image': '../Images/Paris.jpeg',
+    'buttonid': '11LocButton'
+}
+
+let Location12 = {
+    'country': 'EU', 
+    'description': "Swiss Alps",
+    'image': '../Images/Swiss Alps.jpeg',
+    'buttonid': '12LocButton'
+}
+
+let Location13 = {
+    'country': 'EU', 
+    'description': 'Rome',
+    'image': '../Images/Rome.jpeg',
+    'buttonid': '13LocButton'
+}
+
+let Location14 = {
+    'country': 'EU', 
+    'description': 'London',
+    'image': '../Images/London.jpeg',
+    'buttonid': '14LocButton'
+}
+
+let Location15 = {
+    'country': 'EU', 
+    'description': "Amsterdam",
+    'image': '../Images/Amsterdam.jpeg',
+    'buttonid': '15LocButton'
+}
+
+// Africa Locations
+let Location16 = {
+    'country': 'AFR', 
+    'description': 'Serengeti National Park',
+    'image': '../Images/Serengeti National Park.jpeg',
+    'buttonid': '16LocButton'
+}
+
+let Location17 = {
+    'country': 'AFR', 
+    'description': "Victoria Falls",
+    'image': '../Images/Victoria Falls.jpeg',
+    'buttonid': '17LocButton'
+}
+
+let Location18 = {
+    'country': 'AFR', 
+    'description': 'Tanzania',
+    'image': '../Images/Tanzania.jpeg',
+    'buttonid': '18LocButton'
+}
+
+let Location19 = {
+    'country': 'AFR', 
+    'description': 'Mauritius',
+    'image': '../Images/Mauritius.jpeg',
+    'buttonid': '19LocButton'
+}
+
+let Location20 = {
+    'country': 'AFR', 
+    'description': "Masai Mara National Reserve",
+    'image': '../Images/Masai Mara National Reserve.jpeg',
+    'buttonid': '20LocButton'
+}
+
+// Asian Locations
+let Location21 = {
+    'country': 'AS', 
+    'description': 'Maldives',
+    'image': '../Images/Maldives.jpeg',
+    'buttonid': '21LocButton'
+}
+
+let Location22 = {
+    'country': 'AS', 
+    'description': "Tokyo",
+    'image': '../Images/Tokyo.jpeg',
+    'buttonid': '22LocButton'
+}
+
+let Location23 = {
+    'country': 'AS', 
+    'description': 'Phuket',
+    'image': '../Images/Phuket.jpeg',
+    'buttonid': '23LocButton'
+}
+
+let Location24 = {
+    'country': 'AS', 
+    'description': 'Sagarmatha National Park',
+    'image': '../Images/Sagarmatha National Park.jpeg',
+    'buttonid': '24LocButton'
+}
+
+let Location25 = {
+    'country': 'AS', 
+    'description': "Hong Kong",
+    'image': '../Images/Hong Kong.jpeg',
+    'buttonid': '25LocButton'
+}
+// END OF LOCATION OBJECTS
 
 // Put locations in an array
-listoflocations.push(Location1, Location2, Location3, Location4, Location5);
+listoflocations.push(Location1, Location2, Location3, Location4, Location5, Location6, Location7, Location8,
+    Location9, Location10, Location12, Location13, Location14, Location15, Location16,
+    Location17, Location18, Location19, Location20, Location21, Location22, Location23,
+    Location24, Location25
+);
 
 // Function seeks if there is a location in LS(Local Storage), it will change the text in the buttons accordingly
 function loadFavButtons() {
@@ -99,17 +246,32 @@ function loadFavButtons() {
         }
         
         let lsLocationButtonClass = lsLocation.buttonid;
-        
+        // Array of images on the present page
+        let imageonrightpage = document.getElementsByTagName('input');
+        let imagesinarray = [];
+        let imagekey = '';
+        for (let a = 0; a < imageonrightpage.length; a++){
+            imagekey = imageonrightpage[a].className;
+            imagesinarray.push(imagekey);
+
+        }
+        // Match image on page to local storage
+        let isMatch = imagesinarray.some(imagesinarray => imagesinarray === lsKey)
+        console.log(isMatch)
+        if (isMatch === false ){
+            continue;
+        }
         // If the location is in local storage, the text in the box will stay as Unfavorite
         if (localStorage.getItem(lsLocationButtonClass) == null) {
+            console.log(document.getElementsByClassName(lsLocationButtonClass))
             document.getElementsByClassName(lsLocationButtonClass)[0].value = "Favorite";
         }       
         else {
+            console.log(document.getElementsByClassName(lsLocationButtonClass))
             document.getElementsByClassName(lsLocationButtonClass)[0].value = "Unfavorite";
         }  
     }
 }
-
 
 
 // Function sets or deletes locations in LS
@@ -134,6 +296,7 @@ function changeFavButton(clickedButton) {
         clickedButton.value = 'Unfavorite'
         // Put location object inside local storage
         const locationObject = listoflocations[stoppednumberinlist];
+        console.log(locationObject);
         localStorage.setItem(buttonidinlist, JSON.stringify(locationObject));
     }
     else {
@@ -197,7 +360,7 @@ function changeFavButton(clickedButton) {
         modalFavbutton.id = 'favButton';
         modalFavbutton.onclick = function() { changeFavButton(this); };
         
-        // Put created tags inside the modal      
+        // Put created tags inside the modal    
         containter.appendChild(modaltext);
         containter.appendChild(img);  
         containter.appendChild(modalFavbutton);    
